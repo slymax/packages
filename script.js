5458307a1288e5a81e756f5a830fe35c6f57c21f
Max <contact@slymax.com>
2017-06-09 21:49:49 +0200

    update

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 var app = new Vue({
   el: "#app",
   data: {
+    search: "",
     results: {}
   }
 });
-$.getJSON("https://api.npms.io/v2/search?q=keywords:hyperterm&size=100", function(data) {
-  app.results = data.results;
+function search() {
+  $.getJSON("https://api.npms.io/v2/search?q=keywords:hyperterm" + "+" + app.search + "&size=100", function(data) {
+    app.results = data.results;
+  });
+}
+$('input').keypress(function(e) {
+  if (e.which == 13) search();
 });
+search();
